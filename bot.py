e2662eed6bed65349b4a28e32a68bc0aaab13448
Anna <anna.nezdulkina@mail.ru>
2026-02-03 20:45:44 +0700

    change start bot

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import os
+
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
@@ -16,22 +17,25 @@ logger = logging.getLogger(__name__)
 
 
 async def main() -> None:
-    TOKEN_BOT = os.getenv('TOKEN_BOT')
-    bot: Bot = Bot(token=TOKEN_BOT, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    dp: Dispatcher = Dispatcher()
+    TOKEN_BOT = os.getenv("TOKEN_BOT")
+    if not TOKEN_BOT:
+        raise RuntimeError("TOKEN_BOT is not set")
 
-    logging.basicConfig(level=logging.INFO)
-
-    get_async_db()
-
-    dp.include_router(router)
-    dp.include_router(start_router)
-
-    # Логгирование
     logging.basicConfig(
-        level=logging.INFO, filemode='w', filename='runner_tests.log', encoding='UTF-8',
-        format='%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s'
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
     )
+
+    bot = Bot(
+        token=TOKEN_BOT,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+
+    dp = Dispatcher()
+
+    dp.include_router(start_router)
+    dp.include_router(router)
+
     await dp.start_polling(bot)
 
 
