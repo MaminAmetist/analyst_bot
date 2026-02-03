be224d7c670020132ada0b767fd73e392c45e0d8
Anna <anna.nezdulkina@mail.ru>
2026-02-03 19:38:03 +0700

    1

--- create_db.py
+++ create_db.py
@@ -1,16 +1,19 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import DeclarativeBase
 import os
+
 from dotenv import load_dotenv
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Session
 
 load_dotenv()
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 DATABASE_URL = os.getenv('DATABASE_URL')
 
+engine = create_engine(DATABASE_URL, echo=True)
 
-async_engine = create_async_engine(DATABASE_URL, echo=True)
-
-async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
+SessionLocal = sessionmaker(bind=engine)
 
 
 class Base(DeclarativeBase):
--- test_url.py
+++
@@ -1,7 +0,0 @@
-from alembic.config import Config
-
-cfg = Config("alembic.ini")
-url = cfg.get_main_option("sqlalchemy.url")
-
-print("URL repr:", repr(url))
-print("Bytes:", list(url.encode("utf-8")))
