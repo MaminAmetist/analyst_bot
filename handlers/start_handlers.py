4fbde127be5e6391d5684d79045bb1fcf8dca637
Anna <anna.nezdulkina@mail.ru>
2026-02-03 21:27:34 +0700

    add help button

--- handlers/start_handlers.py
+++ handlers/start_handlers.py
@@ -1,18 +1,18 @@
+import os
+
 from aiogram import Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.database import async_engine, Base
-from keyboards.lexicon import MAIN_MENU
 from keyboards.keyboards import create_keys
-
-import os
-
+from keyboards.lexicon import MAIN_MENU
 from services.json_loader import DriveJSONLoader
 
 start_router = Router()
 keyboard_start = create_keys(1, **MAIN_MENU)
+
 
 @start_router.message(CommandStart())
 async def start_handler(message: Message, session: AsyncSession):
@@ -33,3 +33,8 @@ async def start_handler(message: Message, session: AsyncSession):
         await message.answer(f"Загружено видео: {count}")
     except Exception as e:
         await message.answer(f"Ошибка при загрузке данных: {e}")
+
+
+@start_router.message(Command("help"))
+async def help_bot(message: Message):
+    await message.answer(f'Не паникуй, {message.from_user.full_name}, помощь уже в пути.')
